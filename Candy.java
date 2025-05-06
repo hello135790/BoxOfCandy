e3836cf571d5fad897b0b7ea046a103bbdbb8431
Ashley <167535539+hello135790@users.noreply.github.com>
2025-05-06 00:25:51 +0000

    Final with tests cases

--- Candy.java
+++ Candy.java
@@ -1,8 +1,17 @@
 public class Candy {
+    private String flavor;
+
+    public Candy(String f) {
+        flavor = f;
+    }
+
+    public String toString() {
+        return flavor;
+    }
 
     /** Returns a String representing the flavor of this piece of candy */
     public String getFlavor() {
-        /* implementation not shown */ }
+        return flavor;}
 
     // There may be instance variables, constructors, and methods that are not
     // shown.
